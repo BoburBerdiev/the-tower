0de05ce6792f89539dffe62f6e1c17ec2917113f
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-02-29 17:39:59 +0500

    english seo adding

--- src/SEO/SEO.config.js
+++ src/SEO/SEO.config.js
@@ -9,8 +9,8 @@ export const seoContent = {
           description: 'Мы предлагаем высокий уровень сервиса, индивидуальный подход к каждому клиенту, широкий спектр услуг и удобное расположение.',
       },
       en: {
-        title: "THE TOWER HOTEL TASHKENT – это изысканность во всём.",
-        description: "defe"
+        title: 'THE TOWER HOTEL TASHKENT - elegance in everything.',
+        description: 'We offer a high level of service, individual approach to each client, a wide range of services, and convenient location.'
       }
   },
   reviews: {
@@ -32,6 +32,11 @@ export const seoContent = {
         title: 'Виртуальная экскурсия по THE TOWER HOTEL TASHKENT.',
         description: 'Снимки отеля, условии, номера и другие.',
     },
+    en: {
+        title: 'Virtual Tour of THE TOWER HOTEL TASHKENT.',
+        description: 'Photos of the hotel, amenities, rooms, and more.'
+      }
+      
   },
   booking: {
     uz: {
@@ -42,6 +47,11 @@ export const seoContent = {
         title: 'THE TOWER HOTEL TASHKENT – это изысканность во всём.',
         description: 'У нас есть возможность онлайн бронирования.',
     },
+    en: {
+        title: 'THE TOWER HOTEL TASHKENT - Elegance in Everything.',
+        description: 'We offer online booking options.'
+      }
+      
   },
   rooms: {
     uz: {
@@ -52,6 +62,11 @@ export const seoContent = {
         title: 'THE TOWER HOTEL TASHKENT – это изысканность во всём. ',
         description: 'Гости могут насладиться комфортным пребыванием в одном из 96 номеров, оформленных ',
     },
+    en: {
+        title: 'THE TOWER HOTEL TASHKENT - Elegance in Everything.',
+        description: 'Guests can enjoy a comfortable stay in one of our 96 elegantly decorated rooms.'
+      }
+      
   },
   roomsInnerSeo: {
     uz: {
@@ -74,6 +89,11 @@ export const seoContent = {
         title: 'THE TOWER HOTEL TASHKENT  ',
         description: 'Будьте в курсе всех новостей',
     },
+    en: {
+        title: 'THE TOWER HOTEL TASHKENT',
+        description: 'Stay up to date with all the latest news.'
+      }
+      
   },
   newsInner: {
     uz: {
@@ -96,6 +116,11 @@ export const seoContent = {
           title: 'THE TOWER HOTEL TASHKENT это отель премиум класса',
           description: 'который отвечает на все стандарты обслуживания, имеет комфортабельные номера, использует самые передовые технологии и предоставляет широкий спектр дополнительных услуг.',
       },
+      en: {
+        title: 'THE TOWER HOTEL TASHKENT is a premium-class hotel',
+        description: 'that meets all service standards, offers comfortable rooms, utilizes cutting-edge technologies, and provides a wide range of additional services.'
+      }
+      
   },
   contactSEO: {
       uz: {
@@ -106,6 +131,10 @@ export const seoContent = {
           title: 'THE TOWER HOTEL TASHKENT – это изысканность во всём.',
           description: 'Для дополнительной информации свяжитесь с нами.',
       },
+      en: {
+        title: 'THE TOWER HOTEL TASHKENT - Elegance in Everything.',
+        description: 'For additional information, please contact us.'
+      }
   },
 };
 
