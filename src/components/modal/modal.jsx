77b2e40270cf9f67320dfbe00ef50141af47e81f
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-03-01 14:34:17 +0500

    pullucun ;

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className='w-screen h-screen fixed z-[999999] top-0 left-0 right-0 bg-black/50 backdrop-blur-lg'>
+      
+    </div>
+  )
+}
+
+export default Modal
