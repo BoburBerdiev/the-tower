953ec97be80cfc80ca136b24260f5d43ab4ec966
BoburBerdiev <sirofim012@gmail.com>
2024-03-05 10:47:15 +0500

    pull uchuun

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -4,35 +4,37 @@ import { FaRegCircleCheck } from "react-icons/fa6";
 
 const Modal = ({isOpenModal, setIsOpenModal, content}) => {
 
-    setTimeout(() => {
-      setIsOpenModal(false)
+    // setTimeout(() => {
+    //   setIsOpenModal(false)
+    //
+    // }, 5000)
 
-    }, 5000)
-
-    const stopModal = (e) => {
-      e.stopPropagation()
-    }
-    const closeModal = (e) => {
-      setIsOpenModal(false)
-    }
-
-
-  useEffect(() => {
-    if (isOpenModal === true ) {
-      document.body.classList.add('overflow-hidden')
-    }else {
-      document.body.classList.remove('overflow-hidden')
-    }
-  }, [isOpenModal])
+  //   const stopModal = (e) => {
+  //     e.stopPropagation()
+  //   }
+  //   const closeModal = (e) => {
+  //     setIsOpenModal(false)
+  //   }
+  //
+  //
+  // useEffect(() => {
+  //   if (isOpenModal === true ) {
+  //     document.body.classList.add('overflow-hidden')
+  //   }else {
+  //     document.body.classList.remove('overflow-hidden')
+  //   }
+  // }, [isOpenModal])
 
   
 
 
   return (
     <>
-      <section className={`w-screen h-screen overflow-hidden flex flex-col justify-center ${isOpenModal ? "" : "hidden"} fixed z-[9999] top-0 left-0 right-0 bg-black/50 backdrop-blur-lg`} onClick={e => closeModal(e)}>
+      <section className={`w-screen h-screen overflow-hidden flex flex-col justify-center ${isOpenModal ? "" : "hidden"} fixed z-[9999] top-0 left-0 right-0 bg-black/50 backdrop-blur-lg`} >
+        {/*  onClick={e => closeModal(e)} */}
         <div className='container flex flex-col items-center'>
-          <div className='p-10 w-full bg-white rounded shadow-xl sm:w-[60vw] lg:w-[50vw] xl:w-[40vw] ' onClick={e => stopModal(e)}>
+          <div className='p-10 w-full bg-white rounded shadow-xl sm:w-[60vw] lg:w-[50vw] xl:w-[40vw] ' >
+              {/*onClick={e => stopModal(e)}*/}
               <div className='flex flex-col items-center gap-10 '>
                 <div>
                   <FaRegCircleCheck className='w-24 h-24 text-brown'/>
