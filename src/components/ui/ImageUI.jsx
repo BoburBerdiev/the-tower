0ccbd2dca8d0a077ae110bb4f6a5716c242b5ec8
islom <iabdugofurov99@gmail.com>
2024-03-17 19:44:55 +0500

    unoptimized yoqildi

--- src/components/ui/ImageUI.jsx
+++ src/components/ui/ImageUI.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react';
-import dynamic from 'next/dynamic';
+import Image from 'next/image'
+import { useState } from 'react'
 
-const Image = dynamic(() => import('next/image'));
+const ImageUI = ({ src, alt, priority = false, objectFitContain = false, card = false, quality = 90, imageStyle}) => {
 
-const ImageUI = ({ src, alt, priority = false, objectFitContain = false, card = false, quality = 90, imageStyle }) => {
-    const [loading, setLoading] = useState(true);
-
-    const classNames = `${objectFitContain ? 'object-contain' : 'object-cover'} w-full h-full duration-200 ease-in-out ${imageStyle ? imageStyle : 'object-top'} ${loading ? 'scale-110 blur-2xl grayscale' : 'scale-100 blur-0 grayscale-0'}`;
+    const [loading , setLoading] = useState(true)
 
     return (
-        <Image
-            src={src}
-            alt={alt}
-            fill
-            className={classNames}
-            quality={quality}
-            priority={priority}
-            onLoad={() => setLoading(false)}
-            sizes={`${card ? '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' : '100vw'}`}
-        />
-    );
-};
+        <>
+            <Image
+                src={src}
+                alt={alt}
+                fill
+                className={` ${objectFitContain ? 'object-contain' : 'object-cover'}  w-full h-full   duration-200 ease-in-out ${imageStyle ? imageStyle : 'object-top'}  ${
+                    loading ? 'scale-110 blur-2xl grayscale':
+                        'scale-100  blur-0 grayscale-0'
+                } `}
+                quality={quality || 90}
+                priority={priority || false}
+                onLoad={() => setLoading(false)}
+                sizes={`${card ? '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' : '100vw'}`}
 
-export default ImageUI;
+            />
+        </>
+    )
+
+}
+
+export default ImageUI
